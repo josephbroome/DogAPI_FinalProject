b5603e16d493ce2e4d23c6fda3babd0af78bfe99
Joseph Broome <josephlloydbroome@gmail.com>
2022-08-26 07:26:51 -0700

    updates

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogAPI_FinalProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
